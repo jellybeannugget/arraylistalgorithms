674408b208a3c7a1ffa4ad892476a7452a0a478b
jellybeannugget <yusefa2344@gmail.com>
2026-02-04 18:20:02 -0500

    Create ArrayListHW1.java

---
+++ ArrayListHW1.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+ public class ArrayListHW1 { 
+    public static void main(String[] args) { 
+        ArrayList<Integer> nums = new ArrayList<>();
+         for (int i = 0; i < 50; i++) 
+         { int value = (int)(Math.random() * 101); 
+            nums.add(value); } int sum = getSum(nums); 
+            double avg = getAverage(nums); 
+            int max = getMax(nums); 
+            int min = getMin(nums);
+             System.out.println("Sum of ArrayList: " + sum); 
+             System.out.println("Average of ArrayList: " + avg);
+              System.out.println("Maximum value: " + max);
+               System.out.println("Minimum value: " + min); 
+            } 
+            public static int getSum(ArrayList<Integer> list) {
+                 int total = 0; 
+                 for (int i = 0; i < list.size(); i++) {
+                     total += list.get(i); 
+                    } 
+                    return total;
+                 } 
+                 public static double getAverage(ArrayList<Integer> list) { 
+                    return (double)getSum(list) / list.size(); 
+                } 
+                public static int getMax(ArrayList<Integer> list) {
+                     int max = list.get(0); 
+                     for (int i = 1; i < list.size(); i++) { 
+                        if (list.get(i) > max) { 
+                            max = list.get(i);
+                         } 
+                        } 
+                        return max;
+                     } 
+                     public static int getMin(ArrayList<Integer> list) {
+                         int min = list.get(0);
+                          for (int i = 1; i < list.size(); i++) {
+                             if (list.get(i) < min) { 
+                                min = list.get(i);
+                             }
+                             }
+                              return min; 
+                            }
+                        }
